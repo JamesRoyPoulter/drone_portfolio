b84b34dc1e4c626ebf960492c42d4311d02a9ac6
James Roy Poulter <jamesroypoulter@gmail.com>
2014-01-03 20:58:27 +0000

    initial commit

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require underscore
+//= require jquery_nested_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySiteDrone::Application.config.secret_token = '8098dc0dde78a2b63f72e0035748a1ee26202e417a1dd74d89cf50c047a3da0cc00d97c2500d36645d2aa46719a0ff1ec8a176a890a650a06c649c5be1a7fca4'
